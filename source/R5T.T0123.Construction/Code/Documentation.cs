87cd81a97c331c6d73039c51607925ab2b8e9cb7
davidcoats <david@getsafetycone.com>
2022-01-02 12:46:08 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0123.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0123.Construction
+{
+    /// <summary>
+    /// Construction project for IRepositoryPath, IRepositoryPathOperator, and related extension method base types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0123/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0123
+{
+    /// <summary>
+    /// IRepositoryPath, IRepositoryPathOperator, and related extension method base types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
